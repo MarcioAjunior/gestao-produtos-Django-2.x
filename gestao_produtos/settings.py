26859bfedcbcee5f97b48fe92fbfe0228fafbd06
MarcioJr <marciohamer@gmail.com>
2019-01-19 03:00:01 -0200

    segundo commit

--- gestao_produtos/settings.py
+++ gestao_produtos/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['marcio-gestao-produtos.herokuapp.com/']
+ALLOWED_HOSTS = ['marcio-gestao-produtos.herokuapp.com']
 
 
 # Application definition
